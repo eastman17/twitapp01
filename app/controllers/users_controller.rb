2df416f909370487aca9d42eae47150974eaf5a5
Breslau <ruby@eastmt.co.jp>
2015-01-10 16:13:06 +0900

    ch6 mid-flow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def favorites
+    @user = User.find(params[:id])
+  end
+
   # GET /users/new
   def new
     @user = User.new
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   resource :registrations, only: [:new, :create]
   resource :sessions, only: [:new, :create, :destroy]
   resource :settings, only: [:edit, :update]
-  resources :users, only: [:index, :show]
-  resources :tweets
+  resources :users, only: [:index, :show] do
+    get :favorites, on: :member
+  end
+  resources :tweets do
+    resource :favorites, only: [:create, :destroy]
+  end
   root to: 'registrations#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
